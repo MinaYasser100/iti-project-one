3bb87ad2dab1a679bc7089a9bfc878de2830ae04
MinaYasser100 <minayasser43@gmail.com>
2024-07-26 01:39:39 +0300

    add hive in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:one_project_iti/note_app/data/note_model.dart';
 import 'package:one_project_iti/note_app/presentation/views/note_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<List<NoteModel>>('notesBox');
+
   runApp(const MyApp());
 }
 
